f2e953d02d70e4b90a09dd8063e99ca1de5371a2
Jared Clyde <jarclyde@gmail.com>
2022-09-30 11:32:57 -0600

    Practice during squad meeting.

---
+++ Max of Two.js
@@ -0,0 +1,16 @@
+x = 1.1
+y = 1.4
+
+if (x == y) {
+    console.log('The values are identical');
+} else {
+    console.log(Math.max(x, y));
+}
+
+if (x > y) {
+    console.log(x);
+} else if (y > x) {
+    console.log(y);
+} else {
+    console.log('The values are identical');
+}
